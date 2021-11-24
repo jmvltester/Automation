96648a5c8e0ba6af79e98e9204638c8dbe721249
Ravi Juglan <ravijuglan@gmail.com>
2021-11-24 09:12:30 +0000

    First commit for the project

---
+++ DeckOfCards_Test/src/main/java/DeckOfCards_Test/DeckOfCards_Test/App.java
@@ -0,0 +1,13 @@
+package DeckOfCards_Test.DeckOfCards_Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
