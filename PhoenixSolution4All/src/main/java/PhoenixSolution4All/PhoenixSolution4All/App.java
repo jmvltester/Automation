a27212a5c8ccdd548bbce29ecd808c74aa345a3a
Ravi Juglan <ravijuglan@gmail.com>
2021-11-24 23:09:39 +0000

    First Commit for Phoenix Framework

---
+++ PhoenixSolution4All/src/main/java/PhoenixSolution4All/PhoenixSolution4All/App.java
@@ -0,0 +1,13 @@
+package PhoenixSolution4All.PhoenixSolution4All;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
